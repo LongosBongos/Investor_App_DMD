674b74ce5fecce2e7fc1f671b7ae956ce51df018
LongosBongos <pointlessssouls@gmail.com>
2025-08-27 21:59:09 +0200

    fix: valid vite.config.js + correct base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 // vite.config.js
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default {
+// https://vitejs.dev/config/
+export default defineConfig({
   plugins: [react()],
-  // sehr wichtig für GitHub Pages (Repo-Name als Base-Pfad)
+  // WICHTIG für GitHub Pages: Repo-Name als Base-Pfad
   base: '/Investor_App_DMD/',
-}
+})
+
